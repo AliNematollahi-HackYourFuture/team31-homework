e3295346e31e9387baa5c00594b8f211478f3054
Ali Nematollahi <ali.nematollahi2020@gmail.com>
2025-02-03 13:47:25 +0100

    Implemented task 1 (Age-ify)

---
+++ javascript/javascript1/week1/homework-Ali-Nematollahi/Age-ify.js
@@ -0,0 +1,12 @@
+console.log('====== Age-ify ======');
+
+
+let yearOfBirth = 1989;
+let yearFuture = 2050;
+
+let age = yearFuture - yearOfBirth;
+
+console.log('You will be' , age , 'years old in' , yearFuture);
+
+// Better solution
+console.log(`You will be ${age} years old in ${yearFuture}`);
