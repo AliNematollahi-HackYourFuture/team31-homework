f18970a16b5d8e7e246c584411627cef55d5e9b6
Ali Nematollahi <ali.nematollahi2020@gmail.com>
2025-02-11 14:54:31 +0100

    The issues were fixed.

--- javascript/javascript1/week1/homework-Ali-Nematollahi/Age-ify.js
+++ javascript/javascript1/week1/homework-Ali-Nematollahi/Age-ify.js
@@ -6,7 +6,7 @@ const yearFuture = 2050;
 
 const age = yearFuture - yearOfBirth;
 
-console.log("You will be " + age + "' years old in " + yearFuture);
+console.log("You will be " + age + " years old in " + yearFuture);
 
 // Better solution
-console.log(`You will be ${age}' years old in ${yearFuture}`);
+console.log(`You will be ${age} years old in ${yearFuture}`);
