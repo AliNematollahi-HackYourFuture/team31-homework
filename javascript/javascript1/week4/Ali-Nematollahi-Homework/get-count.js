fbf567921eb32be50c4da46cc647a3d05e66a355
Ali Nematollahi <ali.nematollahi2020@gmail.com>
2025-02-25 11:32:21 +0100

    Implemented homework assignment.

---
+++ javascript/javascript1/week4/Ali-Nematollahi-Homework/get-count.js
@@ -0,0 +1,10 @@
+function getCount(str) {
+  let counter = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
+  for (i in str) {
+    if (vowels.includes(str[i])) {
+      counter++;
+    }
+  }
+  return counter;
+}
